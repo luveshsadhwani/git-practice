8f64a0ad65a67a04f75323b112f56021bd2974d6
Luvesh L Sadhwani <luveshsadhwani@gmail.com>
2021-09-28 12:01:07 +0800

    added thirdConst

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 const firstConst = "const for init commit";
 
-const secondConst = "const for a second commit";
+const secondConst = "const for a second commit. This constant will be removed using git reset";
+
+const thirdConst = "const for third commit. This const will be removed using git reset."
